50c23a3f133997171903490642b478bc3f602d6b
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-05-25 17:21:09 +0000

    bye bye

--- js/script.js
+++ js/script.js
@@ -3,3 +3,46 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Assignment-06-Example/sw.js", {
+    scope: "/ICS2O-Assignment-06-Example/",
+  })
+}
+
+
+/**
+ * Get API info.
+*/
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
+
+const getImage = async (URLAddress) => { https://openweathermap.org/img/wn/02d@2x.png
+  try {
+    const result = await fetch(URLAddress )
+    const jsonData = await result.json()
+    console.log(jsonData)
+    document.getElementById("api-image").innerHTML =
+    '<img src="' + 
+      jsonData.url + 
+      '" alt="API image" class="center" ' +
+      '>'
+    if (jsonData.artist_url != "none") {
+      document.getElementById("image-artist").innerHTML =
+      "<p>Artist: " +
+      '<a href="' +
+      jsonData.artist_url +
+      '">' +
+      jsonData.artist +
+      "</a>"
+  } else {
+    document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
+  }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+getImage("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
