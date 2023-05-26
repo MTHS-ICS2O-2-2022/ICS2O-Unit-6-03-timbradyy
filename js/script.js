fd139e8e48c0cdab2a91af7d001a2e3b2acc4d88
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-05-26 17:01:17 +0000

    gravy

--- js/script.js
+++ js/script.js
@@ -1,48 +1,33 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra 
+// Created on: May 2023
 // This file contains the JS functions for index.html
 
-/**
- * Check servie worker.
- */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Assignment-06-Example/sw.js", {
-    scope: "/ICS2O-Assignment-06-Example/",
-  })
-}
+"use strict"
 
-
-/**
- * Get API info.
-*/
-// code from: https://www.youtube.com/watch?v=670f71LTWpM
-
-const getImage = async (URLAddress) => { https://openweathermap.org/img/wn/02d@2x.png
+const getWeather = async (URLAddress) => {
   try {
-    const result = await fetch(URLAddress )
+    const result = await fetch(URLAddress)
     const jsonData = await result.json()
     console.log(jsonData)
+
+    const img = jsonData.weather[0].icon
     document.getElementById("api-image").innerHTML =
-    '<img src="' + 
-      jsonData.url + 
-      '" alt="API image" class="center" ' +
-      '>'
-    if (jsonData.artist_url != "none") {
-      document.getElementById("image-artist").innerHTML =
-      "<p>Artist: " +
-      '<a href="' +
-      jsonData.artist_url +
-      '">' +
-      jsonData.artist +
-      "</a>"
-  } else {
-    document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
-  }
+      '<img src="http://openweathermap.org/img/wn/' +
+      img +
+      '@2x.png" alt="weather image">'
+
+    const tempInC = jsonData.main.temp - 273.15
+    document.getElementById("tempurature").innerHTML =
+      "<p> Tempurature: " + tempInC.toFixed(1) + " °C</p>"
+
+    document.getElementById("weather").innerHTML =
+      "<p> Weather: " + jsonData.weather[0].main + "</p>"
   } catch (err) {
     console.log(err)
   }
 }
-
-getImage("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
+getWeather(
+  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+)
